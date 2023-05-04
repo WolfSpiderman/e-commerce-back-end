392000198d84ca30b14161d55e8d3c00d7c05433
WolfSpiderman <wolfspiderman@yahoo.com>
2023-05-03 22:51:09 -0500

    added starter code, added relationships between models/tables

---
+++ models/index.js
@@ -0,0 +1,45 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
